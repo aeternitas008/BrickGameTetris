a3edc41ac42f5a239a8339a33c9527912497d55f
Louella Them <louellat@pr-i6.kzn.21-school.ru>
2024-09-18 19:46:56 +0300

    renames

---
+++ src/gui/cli/frontend.h
@@ -0,0 +1,23 @@
+#ifndef TETRIS_FRONTEND_H
+#define TETRIS_FRONTEND_H
+
+#include <string.h>
+
+#include "../../brick_game/tetris/defines.h"
+#include "../../brick_game/tetris/objects.h"
+#include "../../brick_game/tetris/tetr_backend.h"
+
+void print_overlay(void);
+void print_rectangle(int top_y, int bottom_y, int left_x, int right_x);
+void print_stats(GameInfo_t *stats);
+void print_field();
+void print_board(board_t map);
+void print_gameover(GameInfo_t *stats);
+void print_tetramino(tetramino_t tetramino);
+void clear_tetramino(tetramino_t tetramino);
+
+void print_next_tetr(tetramino_t tetramino);
+void print_pause();
+void clear_pause();
+void update_time(int hours, int minutes);
+#endif
